e09a64c84fc8bcb0957850ce95f823ea45051fba
diegoduqqe@gmail.com <47634821+dukej10@users.noreply.github.com>
2022-07-04 22:54:53 -0500

    usando botón y useState

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,26 @@
 /*
  Componente de elemento de gasto
 */
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title); // cuando cambié el valor se modifica
+
+  const clickHandler = () => {
+    setTitle("Juan");
+    console.log("Clicked");
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
