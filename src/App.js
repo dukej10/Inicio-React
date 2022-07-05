4ef1f8ea6af2e24bff26c95861a066d98f18c358
diegoduqqe@gmail.com <47634821+dukej10@users.noreply.github.com>
2022-07-05 15:13:02 -0500

    Agregar un nuevo elemento a la lista

--- src/App.js
+++ src/App.js
@@ -1,36 +1,42 @@
+import React, { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
+const INITIAL_EXPENSES = [
+  {
+    id: "e1",
+    title: "Car Insurance",
+    amount: 100,
+    date: new Date(2022, 2, 4),
+  },
+  {
+    id: "e2",
+    title: "Car Repair",
+    amount: 300.3,
+    date: new Date(2022, 7, 3),
+  },
+  {
+    id: "e3",
+    title: "Fines payment",
+    amount: 500.2,
+    date: new Date(2022, 7, 3),
+  },
+  {
+    id: "e4",
+    title: "Car purchased",
+    amount: 1500,
+    date: new Date(2022, 2, 3),
+  },
+];
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Car Insurance",
-      amount: 100,
-      date: new Date(2022, 2, 4),
-    },
-    {
-      id: "e2",
-      title: "Car Repair",
-      amount: 300.3,
-      date: new Date(2022, 7, 3),
-    },
-    {
-      id: "e3",
-      title: "Fines payment",
-      amount: 500.2,
-      date: new Date(2022, 7, 3),
-    },
-    {
-      id: "e4",
-      title: "Car purchased",
-      amount: 1500,
-      date: new Date(2022, 2, 3),
-    },
-  ];
+  const [expenses, setExpenses] = useState(INITIAL_EXPENSES);
 
+  // Agregar nuevo gasto
   const addExpense = (expense) => {
-    console.log("In App.js");
     console.log(expense);
+    setExpenses((prevExpense) => {
+      console.log(prevExpense);
+      return [...prevExpense, expense];
+    });
   };
 
   return (
