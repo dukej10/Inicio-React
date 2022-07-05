bb5fe62a5aafcf30e7ad814ab2d0de000f24529b
diegoduqqe@gmail.com <47634821+dukej10@users.noreply.github.com>
2022-07-04 22:12:13 -0500

    proyecto, card se agregó para evitar duplicidad de código

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -2,9 +2,9 @@
 // children permite que el componente hijo pueda tener código dentro de su interior
 // Si no se puede dejar todo sin sacar algo en común
 import "./Card.css";
-function Card(props) {
+const Card = (props) => {
   const classes = "card " + props.className; // obtener los estilos de los hijos
   return <div className={classes}>{props.children}</div>;
-}
+};
 
 export default Card;
