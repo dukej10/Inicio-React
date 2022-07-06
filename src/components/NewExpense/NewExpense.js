68cdb3a913d333bc48ebe8e84467fac3c0e1f241
diegoduqqe@gmail.com <47634821+dukej10@users.noreply.github.com>
2022-07-05 19:35:58 -0500

    mostrar y ocultar

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,21 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
   const saveExpenseDataHandler = (data) => {
     const expenseData = {
       ...data,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setIsEditing(false);
   };
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  };
+
   return (
     <div className="new-expense">
-      {
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+      {isEditing && (
         <ExpenseForm
           onSaveExponseData={saveExpenseDataHandler}
         /> /*se pasa una función para guardar datos función */
-      }
+      )}
     </div>
   );
 };
