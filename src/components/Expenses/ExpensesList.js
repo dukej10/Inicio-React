d88c50b0b8fc2b04649c8a537093e988446a005a
diegoduqqe@gmail.com <47634821+dukej10@users.noreply.github.com>
2022-07-05 19:38:23 -0500

    corrección If porque es para cuando no contenga gastos para ese año

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -2,7 +2,7 @@ import React from "react";
 import ExpenseItem from "./ExpenseItem";
 import "./ExpensesLlist.css";
 const ExpensesList = (props) => {
-  if (props.items.length > 0) {
+  if (props.items.length === 0) {
     return (
       <h2 className="expenses-list__fallback">No se encontraron gastos</h2>
     );
